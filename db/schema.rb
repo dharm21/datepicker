9e6fdd33bb7f1ab97e2b964aa1e2af2ae43ea49f
dharm21 <dharmendrayuvasoft189@gmail.com>
2017-05-11 12:32:35 +0530

    database changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510173127) do
+ActiveRecord::Schema.define(version: 20170511070137) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
